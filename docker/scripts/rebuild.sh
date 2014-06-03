68227322a63ab312dbd03459a8100762fc4c15e9
Ben Dixon <ben@hillsbede.co.uk>
2014-06-03 22:14:42 +0100

    rebuild script should include path

--- docker/scripts/rebuild.sh
+++ docker/scripts/rebuild.sh
@@ -1,4 +1,4 @@
 docker stop rails
 docker rm rails
-docker build -t rails
+docker build -t rails /app
 docker run -d -p 3000:3000 -v /app:/app --link redis:redis --link postgres:db --name rails rails:latest
